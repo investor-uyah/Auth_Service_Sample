7ed122d4f70dae6d1c7ef99e2ee36df2557d5a85
Mfonobong Uyah <investor.uyah@gmail.com>
2025-09-01 12:04:28 +0100

    added a simple home view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,17 @@
+from django.http import HttpResponse
 from django.urls import path
 from .views import RegistrationView, LoginView
 
+
+# Create your views here. 
+
+
+def home(request):
+    return HttpResponse("Django Auth Service is live. Try '/auth/login' or '/auth/register' or 'api/password_reset' to see available pages.")
+
+
 urlpatterns = [
+    path('', home),
     path('register/', RegistrationView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
 ]
